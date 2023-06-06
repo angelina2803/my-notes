d5004f74000e13ddebce091563e03e9071d87b45
Angelina Yumatova <>
2023-06-06 14:48:02 +0200

    change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import NotesItem from "./components/NotesItem";
 import NotesList from "./components/NotesList";
 import "./App.css";
 
 function App() {
+
   const [posts, setPosts] = useState([
     {id: 1, dateEvent: '22.5.2023', title: 'Going to a restaurant with friends'},
     {id: 2, dateEvent: '24.3.2023', title: 'Going to a restaurant with friends'},
   ])
   return (
     <div className="App">
-    <NotesList posts={posts}/>
+      <NotesList posts={posts}/>
     </div>
   );
 }
